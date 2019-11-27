774dd3358cc1d2fa7ac6d48d1ea421d61ab492d0
VenkataSai <saisiva249@gmail.com>
2019-11-27 22:26:45 +0530

    Created ReverseofaNumber

---
+++ ReverseofaNumber.cs
@@ -0,0 +1,41 @@
+using static System.Console;
+
+namespace SamplePrograms
+{
+    class Program
+    {
+        /// <summary>
+        /// Find the sum of digits in a given number
+        /// if entered value is not a valid number then display message
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WriteLine("Enter a number to find reverse ");
+            if (int.TryParse(ReadLine(), out int i))
+            {
+                int sum = 0;
+
+                while (i > 0)
+                {
+                    sum = sum*10 + i % 10;
+                    i /= 10;
+                }
+                WriteLine("the reverse of given number " + sum);
+            }
+            else
+            {
+                WriteLine("entered number is not a valid integer");
+            }
+            ReadKey();
+        }
+    }
+}
+
+/*learning 
+ * In C# 6 and earlier, you must declare a variable in a separate statement before you pass it as an out argument.
+ * Starting with C# 7.0, you can declare the out variable in the argument list of the method call, rather than in a separate variable declaration.
+ * Use try parse to avoid the casting exceptions thrown by Convert.ToInt() and int.parse() methods
+ */
+
+
